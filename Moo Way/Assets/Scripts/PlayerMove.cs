1b8a3f53890e11b6a0b65ac0cb609ecb4d3eac59
Salomé Pelaez <spsalome@gmail.com>
2020-02-18 15:45:38 -0500

    Player movement script added

---
+++ Moo Way/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKey)
+    }
+}
