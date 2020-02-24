56d7d21e037555dc116d205a88838aa5b3436d36
Salomé Pelaez <spsalome@gmail.com>
2020-02-24 17:31:54 -0500

    Behaviour added

---
+++ Moo Way/Assets/Scripts/EnemyBehavoiur.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavoiur : MonoBehaviour
+{
+    float enemySpeed = 3f;
+    Vector3 direction;
+    Transform target;
+
+    private void Start()
+    {
+        target = FindObjectOfType<Player>().GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        GetTarget();
+    }
+
+    public void GetTarget()
+    {
+        direction = Vector3.Normalize(target.transform.position - transform.position);
+        transform.position += direction * enemySpeed * Time.deltaTime;
+    }
+}
---
+++ Moo Way/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speed = 3.0f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && gameObject.transform.position.y <= 4.1)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) && gameObject.transform.position.x <= 8)
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) && gameObject.transform.position.x >= -8)
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+}
