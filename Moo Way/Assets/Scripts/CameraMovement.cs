ca106c600fa0ae37d71794c4f5f4d76208fb0ebf
cjmm0811 <cjmm0811@gmail.com>
2020-03-09 13:28:00 -0500

    Camera movement added

---
+++ Moo Way/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform playerTr;
+    
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position = new Vector3(playerTr.transform.position.x, playerTr.transform.position.y, transform.position.z);
+    }
+}
