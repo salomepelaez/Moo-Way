8c11038939b436d2f596eff8aabdc7c19d92b9ca
Salomé Pelaez <spsalome@gmail.com>
2020-04-14 23:23:27 -0500

    Cow generator
    Plus sky sprites

---
+++ Moo Way/Assets/Scripts/CowGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowGenerator : MonoBehaviour
+{
+    public GameObject c;
+    
+    void Start()
+    {
+        GenerateCow();
+    }
+
+    public void GenerateCow()
+    {
+        GameObject cow = Instantiate(c, Vector3.zero, Quaternion.identity);
+        Vector3 pos = new Vector3();
+        pos.x = Random.Range(41f, 120f);
+        pos.y= 0.1f;
+    }
+}
