c91fe2d48c9e1038967d4a9cf61ecda7d62877ed
Salomé Pelaez <spsalome@gmail.com>
2020-04-25 21:34:22 -0500

    State machine fixed

--- Moo Way/Assets/FollowBehaviour.cs
+++ Moo Way/Assets/FollowBehaviour.cs
@@ -20,23 +20,5 @@ public class FollowBehaviour : StateMachineBehaviour
         {
             animator.SetBool("isFollowing", false); 
         }
-
-        //animator.transform.position = Vector2.MoveTowards(animator.transform.position, target.position, speed * Time.deltaTime);
     }
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-       
-    }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
---
+++ Moo Way/Assets/Scripts/Cows/CowBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowBehaviour : MonoBehaviour
+{
+    public Transform target;
+    
+    Vector2 direction;
+
+    void Start()
+    {
+        target = Points.points[0];
+    }
+}
