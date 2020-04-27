aebba6a6133f8e7f6f6f99003076b56faf4b1de7
Salomé Pelaez <spsalome@gmail.com>
2020-04-26 19:32:08 -0500

    Text and messages

---
+++ Moo Way/Assets/Scripts/Cows/Cow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow : MonoBehaviour
+{
+    void Awake()
+    {
+        transform.tag = "Cow";
+    }
+}
---
+++ Moo Way/Assets/Scripts/Player/Messages.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Messages : MonoBehaviour
+{
+    public TextMeshProUGUI message;
+    private bool held = false;
+
+    private void Awake()
+    {
+        Intialize();
+    }
+
+    private void Intialize()
+    {
+        message = GameObject.Find("cow_message").GetComponent<TextMeshProUGUI>();
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.tag == "Cow" && held == false)
+        {
+            message.text = "Press Y to pick up the cow";
+
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                message.text = "";
+                held = true;
+            }
+        }
+
+        else if(held == true)
+             message.text = "";
+    }    
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.tag == "Cow")
+        {
+            message.text = "";
+        }
+    }
+}
