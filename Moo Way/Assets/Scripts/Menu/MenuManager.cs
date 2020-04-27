eb84708ca814ad6f9fca5d14c0a8e17a672bdf32
Salomé Pelaez <spsalome@gmail.com>
2020-04-26 20:28:27 -0500

    Buttons and Scene manager

---
+++ Moo Way/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+}
