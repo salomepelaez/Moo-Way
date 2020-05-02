eb73ff30d2f1eb162ef28f0209607361b0beb682
Salomé Pelaez <spsalome@gmail.com>
2020-05-01 20:12:07 -0500

    3 new cameras added

--- Moo Way/Assets/Scripts/Camera/CinemachineScript.cs
+++ Moo Way/Assets/Scripts/Camera/CinemachineScript.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CinemachineScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject virtualCamera1;
+    public GameObject virtualCamera2;
+    public GameObject virtualCamera3;
+
+    void Awake()
     {
-        
+        virtualCamera1.SetActive(false);
+        virtualCamera2.SetActive(false);
+        virtualCamera3.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.tag == "Collider1")
+        {
+            virtualCamera1.SetActive(true);
+        }        
+
+        if(other.tag == "Collider2")
+        {
+            virtualCamera2.SetActive(true);
+        }   
+
+        if(other.tag == "Collider3")
+        {
+            virtualCamera3.SetActive(true);
+        }   
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Collider1")
+        {
+            virtualCamera1.SetActive(false);
+        }  
+
+        if(other.tag == "Collider2")
+        {
+            virtualCamera2.SetActive(false);
+        }   
+
+        if(other.tag == "Collider3")
+        {
+            virtualCamera3.SetActive(false);
+        }       
     }
 }
