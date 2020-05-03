7b55cedd2bb49989cc384725f075c8a8c8b6812d
Salomé Pelaez <spsalome@gmail.com>
2020-05-02 21:23:25 -0500

    Intro dialogue

---
+++ Moo Way/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI intro;
+    public string[] phrases;
+    int index;
+    public GameObject button;
+
+    private void Start()
+    {
+        First();        
+    }    
+
+    public void First()
+    {
+        intro.text = phrases[0];
+    }
+
+    IEnumerator PrintMessages()
+    {
+        foreach (char p in phrases[index].ToCharArray())
+        {
+            intro.text += p;
+        }
+
+        yield return new WaitForSeconds(0.2f);
+    }
+
+    public void NextSentence()
+    {
+        if(index < phrases.Length - 1)
+        {
+            index++;
+            intro.text = "";
+
+            StartCoroutine(PrintMessages());            
+        }
+
+        else
+            intro.text = "";
+
+        if (index == phrases.Length - 1)
+        {
+            button.SetActive(false);  
+            intro.text = "";                             
+        }        
+    }
+}
---
+++ Moo Way/Assets/Scripts/Dialogue/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Messages : MonoBehaviour
+{
+    public TextMeshProUGUI continueB;
+
+    public GameObject continueButton;
+
+    private void Awake()
+    {
+        continueB = GameObject.Find("ContinueB").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Continue()
+    {
+        continueB.text = "";
+        continueButton.SetActive(false);
+    }
+}
