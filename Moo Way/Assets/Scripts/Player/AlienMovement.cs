16dbe85ee4b84c05be534676d92c0a3f8906a4cb
Salomé Pelaez <spsalome@gmail.com>
2020-05-09 16:07:08 -0500

    Transitions between animations

--- Moo Way/Assets/Scripts/Player/AlienMovement.cs
+++ Moo Way/Assets/Scripts/Player/AlienMovement.cs
@@ -9,12 +9,20 @@ public class AlienMovement : MonoBehaviour
     public Joystick joystick;
 
     private Vector2 direction;
-    
+
     public static bool alienControl = false;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        ChangeControl();   
+        ChangeControl();
+        KeyboardMovement();   
     }
 
     void ChangeControl()
@@ -23,6 +31,28 @@ public class AlienMovement : MonoBehaviour
         {
             direction = joystick.Direction * speed * Time.deltaTime;
             transform.position += new Vector3(direction.x, direction.y, 0f);
+        }
+    }
+
+    void KeyboardMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+            transform.localScale= new Vector2(0.22349f, 0.1635545f);
+            anim.SetBool("walking", true);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+            transform.localScale= new Vector2(-0.22349f, 0.1635545f);
+            anim.SetBool("walking", true);
+        }
+
+        else
+        {
+            anim.SetBool("walking", false);            
         }
     }
 }
