4781648a121d2aae0dd2a71c37fdff7ab67d878a
Salomé Pelaez <spsalome@gmail.com>
2020-06-24 23:23:24 -0500

    Instructions

--- Moo Way/Assets/Scripts/Menu/MenuManager.cs
+++ Moo Way/Assets/Scripts/Menu/MenuManager.cs
@@ -27,4 +27,9 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene("FirstLevel");
     }
 
+    public void Instructions()
+    {
+        startLevel.Play();
+        SceneManager.LoadScene("Instructions");
+    }
 }
